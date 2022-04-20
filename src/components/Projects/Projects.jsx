b7c193db5d072c5353655ede829b1431f5d42c53
DSV68609 <dominicvillanueva07@gmail.com>
2022-04-20 12:02:26 +0800

    projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Projects.css'
-import { Project_frontend } from '../../containers'
+import { Project_frontend } from '../../containers';
+import { Project_backend } from '../../containers';
 
 
 const Projects = () => {
   return (
     <div className="container">
         <Project_frontend/>
+        <Project_backend/>
     </div>
   )
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as BgAnimation } from './BgAnimation/BgAnimation';
 export { default as Carousel } from './Carousel/Carousel';
 export { default as Project_frontend } from './Project_frontend/Project_frontend';
+export { default as Project_backend } from './Project_backend/Project_backend';
