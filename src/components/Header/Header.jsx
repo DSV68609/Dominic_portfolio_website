a9b35e39fdfc7bf36cd9124bcaa5f72cd393e551
DSV68609 <dominicvillanueva07@gmail.com>
2022-04-22 06:00:51 +0800

    icons update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 import { Typewriter } from "react-simple-typewriter";
 import facebook from '../../assets/facebook.png';
@@ -10,6 +10,13 @@ import { BgAnimation, Carousel } from '../../containers';
 
 
 const Header = () => {
+  const [isFb, setIsFb ] = useState(false);
+  const [isHub, setIsHub ] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isGram, setIsGram ] = useState(false);
+  const [isTweet, setIsTweet ] = useState(false);
+  const [isContact, setIsContact ] = useState(false);
+
   return (
     <div className="header section__padding" id="home">
       <div className="header-content">
@@ -28,14 +35,54 @@ const Header = () => {
         </h1>
         <Carousel/>
         <div className="header-content__input">
-          <button type="button">Hire Me!</button>
+          <button onClick={() => setIsContact(!isContact)} type="button">Contact and Hire Me!</button>
+          {isContact &&
+            <div className="contact_icon">
+              <p>Number: 09915708880</p>
+            </div>
+          }
         </div>
         <div className="icon_link">
-          <a><img src={gmail} alt="gmail"/></a>
-          <a><img src={github} alt="github"/></a>
-          <a><img src={facebook} alt="facebook"/></a>
-          <a><img src={instagram} alt="instagram"/></a>
-          <a><img src={twitter} alt="twitter"/></a>
+          <div>
+            <a><img src={gmail} onClick={() => setIsOpen(!isOpen)} alt="gmail"/> </a>
+            {isOpen && 
+            <div className="gmail_icon">
+              <p>dominicvillanueva07@gmail.com</p>
+            </div>
+            }
+          </div>
+          <div>
+            <a><img src={github} onClick={() => setIsHub(!isHub)} alt="github"/></a>
+            {isHub &&
+              <div className="github_icon">
+                  <p>DSV68609</p>
+              </div>
+            }
+          </div>
+          <div>
+            <a><img src={facebook} onClick={() => setIsFb(!isFb)} alt="facebook"/></a>
+            {isFb && 
+              <div className="fb_icon">
+                <p>Dominic Soriano Villanueva</p>
+              </div>
+            }
+          </div>
+          <div>
+            <a><img src={instagram} onClick={() => setIsGram(!isGram)} alt="instagram"/></a>
+            {isGram && 
+              <div className="insta_icon">
+                <p>@Boniknoks</p>
+              </div>
+            }
+          </div>
+          <div>
+            <a><img src={twitter} onClick={() => setIsTweet(!isTweet)} alt="twitter"/></a>
+            {isTweet && 
+              <div className="tweet_icon">
+                <p>@DominicEinstein</p>
+              </div>
+            }
+          </div>
         </div>
         <p>Click icons to show my accounts!</p>
       </div>
